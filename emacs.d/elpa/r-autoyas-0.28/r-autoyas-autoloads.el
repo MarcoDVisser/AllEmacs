858d9e85ee5e58b773135a7888b52b0c31493723
Marco Visser <marco.d.visser@gmail.com>
2013-12-18 14:45:14 +0100

    updated to emacs 24

---
+++ emacs.d/elpa/r-autoyas-0.28/r-autoyas-autoloads.el
@@ -0,0 +1,28 @@
+;;; r-autoyas-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (r-autoyas-ess-activate) "r-autoyas" "r-autoyas.el"
+;;;;;;  (21066 60321))
+;;; Generated autoloads from r-autoyas.el
+
+(autoload 'r-autoyas-ess-activate "r-autoyas" "\
+R autoyas ESS hook
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("r-autoyas-pkg.el") (21066 60321 888981))
+
+;;;***
+
+(provide 'r-autoyas-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; r-autoyas-autoloads.el ends here
